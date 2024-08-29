6010a5157f14802a93cc7a806915ea830ce8191d
b1gd3vd0g <devin@bigdevdog.com>
2024-08-28 22:43:44 -0700

    Create the express app, configure cors, and use the externally defined base router.

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: true,
+    credentials: true
+  })
+);
+
+app.use('/', require('./routers/base_router'));
+
+module.exports = app;
